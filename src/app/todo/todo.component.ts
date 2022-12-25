6afc87ba0d51b2e1cb87b99ada6d6746ac31831d
Iftekharul Alam <iftekharul.alam@selise.ch>
2022-12-25 11:06:20 +0600

    some edge cases handled

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -25,6 +25,7 @@ export class TodoComponent implements OnInit {
   }
 
   drop(event: CdkDragDrop<ITask[]>) {
+    if(this.isEditEnable) return ;
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
